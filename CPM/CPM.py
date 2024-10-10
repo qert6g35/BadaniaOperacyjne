896ecf4742b1a2b404c1492e47a448333923358d
Piotr Kulczycki <piotr9833@gmail.com>
2024-10-10 14:14:39 +0200

    folder

---
+++ CPM/CPM.py
@@ -0,0 +1,35 @@
+
+V = [5,5,5]
+E = [[0,1],[1,2]]
+
+
+def CPM(V,E):
+    finish = False
+    ES = [0] * len(V) #Early Start
+    EF = [0] * len(V) #Early Finish
+    LS = [0] * len(V) #Late Start
+    LF = [0] * len(V) #LateFinish
+    print("ES")
+    print(ES)
+    print("EF")
+    print(EF)
+    while(finish == False):
+        finish = True
+        for v_i in range(0,len(V)): # v_i - V index 
+            for e in E:
+                if e[1] == v_i:
+                    ES[v_i] = max(ES[v_i],EF[e[0]])
+            EF[v_i] = ES[v_i] + V[v_i]
+    print("PostCPM")
+    print("ES")
+    print(ES)
+    print("EF")
+    print(EF)
+        
+
+    
+                
+
+
+
+CPM(V,E)
