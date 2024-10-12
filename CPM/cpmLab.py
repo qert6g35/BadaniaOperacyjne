61699a2b0344c902d318740be759a86a579030c3
szolkiewicz <s.d.zolkiewicz@gmail.com>
2024-10-12 15:44:49 +0200

    sortowanie topologiczne

--- CPM/cpmLab.py
+++ CPM/cpmLab.py
@@ -1,21 +1,66 @@
+import numpy as np
+from pathlib import Path
+from collections import defaultdict
+
 # nazwa pliku bez dopisku .txt
 # czasy i krawędzie jako listy
 def loadData(filename):
-    f = open(filename+".txt", "r")
-    f.readline()
-    timesraw = f.readline()[:-1]
-    timesraw = timesraw.split(" ")
-    times = [int(timesraw[i]) for i in range(0,len(timesraw))]
     
-    edgesraw = f.readline()[:-1]
-    edgesraw = edgesraw.split("  ")
-    edgesraw = [edgesraw[i].split(" ") for i in range(0,len(edgesraw))]
-    edges = []
-    for j in range(0,len(edgesraw)):
-        edges.append([int(edgesraw[j][i]) for i in range(0,len(edgesraw[j]))])
+    my_file = Path("./"+filename+".txt")
+    
+    if not my_file.is_file():
+        my_file = Path("./CPM/"+filename+".txt")
 
-    print("times: ", times)
-    print("edges: ", edges)
+    f = open(my_file, "r")
+    f.readline()
+
+    timesraw = f.readline()
+    times = np.fromstring(timesraw,sep=" ").astype(np.int16)
+    
+    edgesraw = f.readline()
+    edgesraw = np.fromstring(edgesraw,sep=" ").astype(np.int16)-1
+    edges = edgesraw.reshape((-1,2),order='C')
+
+    # print("times: ", times)
+    # print("edges: ", edges)
     return times, edges
 
 
+
+
+def topologicalSortKahn(V,E):
+    indegree = defaultdict(int)
+    N = [i for i in range(0,len(V))]
+    Q = []
+    TO = []
+    
+    for e in E[:,1]:
+        indegree[e] += 1
+
+    for n in N:
+        if indegree[n] == 0:
+            Q.append(n)
+
+    while Q:
+        node = Q.pop(0)
+        TO.append(node)
+
+        neighbours = []
+        for e in E:
+            if e[0] == node:
+                neighbours.append(e[1])
+
+        for n in neighbours:
+            indegree[n] -= 1
+            if indegree[n] == 0:
+                Q.append(n)
+
+
+    newV = [V[i] for i in TO]
+    newE = np.array([E[i] for i in TO])
+    return newV, newE, TO
+
+# V,E = loadData("data10")
+# V,E, TO = topologicalSortKahn(V,E)
+
+# print(TO)
